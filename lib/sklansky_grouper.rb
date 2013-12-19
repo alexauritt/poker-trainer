eb6f6500d78e696eb513d48a90ee76b50b141b7f
Alex Auritt <alexauritt@yahoo.com>
2013-12-19 11:38:33 -0500

    refactoring

--- lib/sklansky_grouper.rb
+++ lib/sklansky_grouper.rb
@@ -2,26 +2,7 @@ class SklanskyGrouper
 	def self.group_number(card1, card2)
 		val = nil
 		if is_pair?(card1, card2)
-			case card1.value
-				when :ace, :king, :queen, :jack
-					1
-				when 14..11
-					1
-				when 10
-					2
-				when 9
-					3
-				when 8
-					4
-				when 7, 6
-					5
-				when 5
-					6
-				when 4, 3, 2
-					7
-				else
-					nil
-				end
+			pair_rating(card1.value)
 		else 
 			nil
 		end
@@ -31,5 +12,28 @@ class SklanskyGrouper
 		c1.value == c2.value
 	end
 
-	private_class_method :is_pair?
+	def self.pair_rating(card_value)
+		case card_value
+			when :ace, :king, :queen, :jack
+				1
+			when 14..11
+				1
+			when 10
+				2
+			when 9
+				3
+			when 8
+				4
+			when 7, 6
+				5
+			when 5
+				6
+			when 4, 3, 2
+				7
+			else
+				nil
+			end
+	end
+
+	private_class_method :is_pair?, :pair_rating
 end
